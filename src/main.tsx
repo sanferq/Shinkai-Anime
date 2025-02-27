e91a3b6db2b79791614ffa3ea8a0001fe41a3c3b
sanferq <proselkinmaksim@gmail.com>
2025-02-27 21:14:37 +0300

    efww

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,18 @@
 
 import { createRoot } from "react-dom/client";
 import App from "./App";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter as Router } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import React from "react";
 import './index.css';
 
 
+
+
 createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <Router>
       <App />
-    </BrowserRouter>
+    </Router>
   </React.StrictMode>
 );
